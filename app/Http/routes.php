5c61cf9a671ce547a3f6a42cc919163783f0a693
Firdaus Fauzi <dausfauzi13@gmail.com>
2018-04-19 16:19:31 +0800

    users section complete

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,16 +21,26 @@ Route::auth();
 
 Route::get('/home', 'HomeController@index');
 
-//Route::get('/test', function(){
-//    return User::find(1)->role->name;
-//});
-
 
 Route::get('/admin', function(){
-
     return view('admin.index');
+});
 
+
+Route::group(['middleware'=>'admin'], function(){
+
+//    Route::get('/admin', function(){
+//
+//        return view('admin.index');
+//
+//    });
+
+    Route::resource('admin/users', 'AdminUsersController');
+
+    Route::resource('admin/posts', 'AdminPostsController');
 
 });
 
-Route::resource('admin/users', 'AdminUsersController');
+
+//Route::resource('admin/users', 'AdminUsersController');
+
